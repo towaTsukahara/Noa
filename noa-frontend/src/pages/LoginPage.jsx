d67ee983b49c212db4ac0c5ecc53b612cbee5635
WIN-QH9R4VAI45E\traininguser <iroha.kawasaki@skywill.jp>
2026-06-02 10:54:05 +0900

    ログイン画面の最小構成

--- noa-frontend/src/pages/LoginPage.jsx
+++ noa-frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,59 @@
-function LoginPage() {
+function App() {
   return (
-    <div style={{ padding: 16 }}>
-      <h1>ログイン</h1>
-      <p>（ここに後でログインフォームを作ります）</p>
+    <div
+      style={{
+        minHeight: "100vh",
+        padding: "30px",
+      }}
+    >
+      <div
+        style={{
+          textAlign: "center",
+          paddingTop: "40px",
+        }}
+      >
+        <h1>ログイン</h1>
+
+        <p>Noaアカウントにログイン</p>
+
+        <input
+          type="text"
+          placeholder="メールアドレス or 社員番号"
+          style={{
+            display: "block",
+            width: "500px",
+            height: "50px",
+            margin: "30px auto",
+          }}
+        />
+
+        <input
+          type="password"
+          placeholder="パスワード"
+          style={{
+            display: "block",
+            width: "500px",
+            height: "50px",
+            margin: "30px auto",
+          }}
+        />
+
+        <button
+          style={{
+            width: "500px",
+            height: "50px",
+          }}
+        >
+          ログイン
+        </button>
+
+        <p style={{ marginTop: "20px" }}>
+          アカウントをお持ちでない方は
+          <a href="#">新規登録</a>
+        </p>
+      </div>
     </div>
   );
 }
 
-export default LoginPage;
+export default App;
