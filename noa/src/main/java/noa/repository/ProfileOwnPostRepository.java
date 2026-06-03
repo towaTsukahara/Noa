0c4dd2240f62dd697bf07ee8aedb54d70c991dcc
WIN-KLV03NP4M9L\traininguser <yusuke.nomura@skywill.jp>
2026-06-03 16:17:41 +0900

    俺のバックエンド系削除

--- noa/src/main/java/noa/repository/ProfileOwnPostRepository.java
+++
@@ -1,8 +0,0 @@
-package noa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import noa.entity.ProfileOwnPost;
-
-public interface ProfileOwnPostRepository extends JpaRepository<ProfileOwnPost, Long>{
-
-}
