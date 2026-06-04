a85d8821bbca945bc4e421956eb87dc404815818
WIN-46N3D48U830\traininguser <tomoki.nishida@skywill.jp>
2026-06-04 16:27:35 +0900

    前回と同じ作業途中

--- noa/src/main/java/noa/entity/Tag.java
+++ noa/src/main/java/noa/entity/Tag.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "tags")
 public class Tag {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,23 +17,9 @@ public class Tag {
     @Column(name = "created_at", nullable = false, insertable = false, updatable = false)
     private OffsetDateTime createdAt;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public OffsetDateTime getCreatedAt() {
-        return createdAt;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public OffsetDateTime getCreatedAt() { return createdAt; }
 }
