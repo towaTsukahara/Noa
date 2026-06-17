3c95429c8382f451d912453f8de60018ce2a82ad
WIN-KLV03NP4M9L\traininguser <yusuke.nomura@skywill.jp>
2026-06-17 14:54:20 +0900

    タグ編集画面、検索欄で新規作成

--- noa-frontend/src/pages/TagDetailPage.jsx
+++ noa-frontend/src/pages/TagDetailPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useSearchParams, useNavigate } from "react-router-dom";
 import { relativeTime } from "../utils/relativeTime";
 import MiniPostCard from "../components/post/MiniPostCard";
 import "./TagDetailPage.css";
